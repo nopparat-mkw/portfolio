63b35ce97bfe0a0d498299e8c6985e39d3e64c3b
nopparat <nopparat.mueangkaew@gmail.com>
2020-06-13 20:04:50 +0700

    Updates

---
+++ precache-manifest.1963f6d727644b28afaae57ccfc0d5bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "354cb80dedf2397e002066c0a0535d0d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "92a80ff8fcbe0777e179",
+    "url": "/portfolio/static/css/main.7624a9a4.chunk.css"
+  },
+  {
+    "revision": "3d5cc10b676717aac3e2",
+    "url": "/portfolio/static/js/2.a21113e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.a21113e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92a80ff8fcbe0777e179",
+    "url": "/portfolio/static/js/main.02424ed4.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  }
+]);
